352b108cfdfb3a899aacf0e6773c9a55b52e97f2
Victor Berger <victor.berger@m4x.org>
2015-07-13 16:07:41 +0200

    Integrate derivatives into activation functions.

--- src/activations.rs
+++ src/activations.rs
@@ -2,23 +2,58 @@
 
 use num::{Float, one, zero};
 
+pub struct ActivationFunction<F, V, D>
+    where F: Float,
+          V: Fn(F) -> F,
+          D: Fn(F) -> F
+{
+    _marker: ::std::marker::PhantomData<F>,
+    pub value: V,
+    pub derivative: D
+}
+
+impl<F, V, D> ActivationFunction<F, V, D>
+    where F: Float,
+          V: Fn(F) -> F,
+          D: Fn(F) -> F
+{
+    pub fn new(value: V, derivative: D) -> ActivationFunction<F, V, D> {
+        ActivationFunction {
+            _marker: ::std::marker::PhantomData,
+            value: value,
+            derivative: derivative
+        }
+    }
+}
+
 /// Identity function, do not change its input.
 ///
 /// Very bad for training but can be useful for debugging, or in some
 /// special cases.
-pub fn identity<F: Float>(x: F) -> F { x }
+pub fn identity<F: Float>() -> ActivationFunction<F, fn(F) -> F, fn(F) -> F> {
+    ActivationFunction::new(identity_val, identity_der)
+}
+
+fn identity_val<F: Float>(x: F) -> F { x }
+fn identity_der<F: Float>(_x: F) -> F { one() }
 
 /// Sigmoid function. A classic smooth learning function.
 ///
 /// Its values are `0.0` at `-inf`, `0.5` at `0` and `1.0` at `+inf`
-pub fn sigmoid<F: Float>(x: F) -> F {
-    one::<F>() / ( one::<F>() + x.exp() )
+pub fn sigmoid<F: Float>() -> ActivationFunction<F, fn(F) -> F, fn(F) -> F> {
+    ActivationFunction::new(sigmoid_val, sigmoid_der)
 }
+
+fn sigmoid_val<F: Float>(x: F) -> F { one::<F>() / ( one::<F>() + x.exp() ) }
+fn sigmoid_der<F: Float>(x: F) -> F { -x.exp() / ( one::<F>() + x.exp() ).powi(2) }
 
 /// Step function. Cannot be used for learning, but can be used
 /// to normalize data.
 ///
 /// It outputs `1.0` if input was positive, and `-0.0` if input was negative.
-pub fn step<F: Float>(x: F) -> F {
-    if x.is_sign_positive() { one() } else { zero() }
+pub fn step<F: Float>() -> ActivationFunction<F, fn(F) -> F, fn(F) -> F> {
+    ActivationFunction::new(step_val, step_der)
 }
+
+fn step_val<F: Float>(x: F) -> F { if x.is_sign_positive() { one() } else { zero() } }
+fn step_der<F: Float>(_x: F) -> F { zero() }
